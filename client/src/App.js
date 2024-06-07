379a07f252bef15bab80db77d1627aa9a050576f
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-06-06 22:12:22 -0500

    Added to home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,19 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="relative z-0">
+        <img className="w-full" src="/images/Home.png" alt="Image of a house"></img>
+        <div className="bg-zinc-800 bg-opacity-100 relative bottom-0 right-0 px-4 py-8 md:absolute md:bg-opacity-80 md:py-12">
+          <p className="text-xl text-white text-center md:text-3xl">LIMITLESS POTENTIAL 
+          FOR ANY JOB IMAGINABLE</p>
+        </div>
+      </div>
+      <div className="
+        flex flex-wrap justify-center items-center m-auto max-w-screen-lg
+        font-bold sm:justify-between gap-4 sm:gap-0 px-2 sm:px-12 text-center mt-4">
+        <p className=" text-xl md:text-3xl">Get A Fast, Free Quote Today! </p>
+        <button className="border border-4 border-yellow-500 bg-transparent text-yellow-500 p-2 px-4 sm:px-12 text-md md:text-xl  ">Get A Quote</button>
+      </div>
     </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,13 +6,13 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
   return (
-    <div className="navbar bg-gray-800 text-white">
+    <div className="navbar bg-zinc-800 text-white relative z-10">
     <div className="navbar-start">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-800 text-white rounded-box w-52">
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-zinc-800 text-white rounded-box w-52">
             <li><Link to="/">Home</Link></li>
           <li>
             <a>Services</a>
@@ -25,7 +25,7 @@ export default function Header() {
           <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
-      <a className="btn btn-ghost text-xl"><Link to="/">Dahl Builders</Link></a>
+      <Link to="/" className="btn btn-ghost text-xl">Dahl Builders</Link>
     </div>
     <div className="navbar-center hidden lg:flex ">
       <ul className="menu menu-horizontal px-1">
@@ -33,7 +33,7 @@ export default function Header() {
         <li>
           <details>
             <summary>Services</summary>
-            <ul className="p-2 bg-gray-800 rounded-b-lg rounded-t-none">
+            <ul className="p-2 bg-zinc-800 rounded-b-lg rounded-t-none">
               <li><a>Submenu 1</a></li>
               <li><a>Submenu 2</a></li>
             </ul>
