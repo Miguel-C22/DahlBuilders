97cbe307dd18dfd8d9ef47d8b98aa71f43b6947e
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-06-12 20:12:37 -0500

    Added more to home page and made a btn component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Services from "./components/Services";
+import YellowQuoteBtn from "./components/YellowQuoteBtn";
 
 function App() {
   return (
@@ -16,9 +17,23 @@ function App() {
         flex flex-wrap justify-center items-center m-auto max-w-screen-lg
         font-bold sm:justify-between gap-4 sm:gap-0 px-2 sm:px-12 text-center my-8">
         <p className=" text-xl md:text-3xl">Get A Fast, Free Quote Today! </p>
-        <button className="border border-4 border-yellow-500 bg-transparent text-yellow-500 p-2 px-4 sm:px-12 text-md md:text-xl  ">Get A Quote</button>
+        <YellowQuoteBtn />
       </div>
         <Services />
+      <div className="flex flex-col items-center gap-12 my-8">
+        <h2 className="text-2xl font-bold underline underline-offset-8 decoration-yellow-500">MN’s Trusted Remodeler & General Contractor</h2>
+        <div className="flex gap-12 px-24 flex-wrap md:flex-nowrap">
+          <div>
+            <h3 className="text-lg font-bold">Licensed and Insured General Contractor</h3>
+            <p>Insured and covered, we have been a trusted contractor in the Twin Cities Metro for many years. Our crews have over 25 years of experience in floor remodeling and construction. MN homeowners love the results when they choose to hire Massoglia Contracting.</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-bold">Serving in the Twin Cities</h3>
+            <p>Based in Blaine, MN we will travel within a 50-mile radius of our home office location. For the right project, we will go even further. Our five-year craftsman’s guarantee is our commitment to you that we will get the job done right.</p>
+          </div>
+        </div>
+          <YellowQuoteBtn />      
+        </div>
     </div>
   );
 }
---
+++ client/src/components/YellowQuoteBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function YellowQuoteBtn() {
+  return (
+    <div>
+        <button className='btn border border-4
+         border-yellow-500 bg-transparent text-yellow-500 
+         font-bold text-md md:text-xl'
+         >Get a Quote</button>
+    </div>
+  )
+}
+
+export default YellowQuoteBtn
