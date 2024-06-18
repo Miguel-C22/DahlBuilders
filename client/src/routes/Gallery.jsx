5ff7119c22afdc9772e2ca3b6e937b76731b4ac0
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-06-17 19:33:00 -0500

    Gallery update

--- client/src/routes/Gallery.jsx
+++ client/src/routes/Gallery.jsx
@@ -7,13 +7,13 @@ function Gallery() {
     <div>
         <Header /> 
         <div className="relative z-0 pt-16">
-            <img className="h-48 w-full sm:h-auto " src="/images/gallery/galleryHeaderImage.png" alt="Image of a house"></img>
-            <div className="absolute bottom-0 left-2 px-4 py-8 lg:bottom-24 md:py-24 sm:left-24">
-                <h3 className="text-2xl text-white text-left md:text-3xl">Project Gallery </h3>
-                <p className='text-white'>Check out some of our recent work. <br/> Bathroom remodeling, kitchen remodeling, flooring, decks, fencing and more!</p>
-            </div>
-        </div>
-        <div className='flex flex-wrap gap-12 justify-center pt-12'>
+    <img className="w-full h-auto object-cover sm:max-h-screen md:max-h-[70vh] lg:max-h-screen 2xl:max-h-[50vh]" src="/images/gallery/galleryHeaderImage.png" alt="Image of a house" />
+    <div className="absolute bottom-0 left-2 px-4 py-8 lg:bottom-24 md:py-24 sm:left-24">
+        <h3 className="text-4xl text-white text-left md:text-6xl">Project Gallery</h3>
+        <p className="text-white pt-0 hidden sm:pt-2 sm:block">Check out some of our recent work. <br /> Bathroom remodeling, kitchen remodeling, flooring, decks, fencing and more!</p>
+    </div>
+</div>
+        <div className='flex flex-wrap gap-12 justify-center pt-12 px-4'>
             <img src="/images/bathrooms.png" alt="" srcset="" />
             <img src="/images/decks.png" alt="" srcset="" />
             <img src="/images/roofing.png" alt="" srcset="" />
@@ -38,3 +38,4 @@ function Gallery() {
 }
 
 export default Gallery
+//h-48 w-full sm:h-auto
