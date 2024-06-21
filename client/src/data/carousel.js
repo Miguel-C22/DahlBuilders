a20ae25c2dd273b7ddfc6eda0543a77282585f68
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-06-20 21:00:19 -0500

    carousel update

--- client/src/data/carousel.js
+++ client/src/data/carousel.js
@@ -1,21 +1,27 @@
 const carouselImages = [
     {
        Img: "/images/carousel/image1.png",
+       text: "LIMITLESS POTENTIAL FOR ANY JOB IMAGINABLE"
     },
     {
         Img: "/images/carousel/image2.png",
+        text: "TRANSFORM YOUR SPACE INTO A MASTERPIECE"
      },
      {
         Img: "/images/carousel/image3.png",
+        text: "FROM DREAMS TO REALITY, WE BUILD IT ALL"
      },
      {
         Img: "/images/carousel/image4.png",
+        text: "INNOVATIVE DESIGNS, EXCEPTIONAL CRAFTSMANSHIP"
      },
      {
         Img: "/images/carousel/image5.png",
+        text: "YOUR VISION, OUR EXPERTISE – REMODELING PERFECTION"
      },
      {
         Img: "/images/carousel/image6.png",
+        text: "EXCEEDING EXPECTATIONS, ONE HOME AT A TIME"
      },
     
 ]
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,13 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: { height: {
+    extend: { 
+      height: {
       'tall': '42rem'
-    }},
+    },
+    textShadow: {
+      'dark-lg': '2px 2px 4px rgba(0, 0, 0, 1)', // Customize the shadow as needed
+    },
+  },
   },
   daisyui: {
     themes: [],
  },
-  plugins: [require('daisyui'),],
+  plugins: [require('daisyui'), function({ addUtilities }) {
+    addUtilities({
+      '.text-shadow-dark-lg': {
+        'text-shadow': '2px 2px 4px rgba(0, 0, 0, 1)', // Customize the shadow as needed
+      },
+    })
+  }],
 }
 
